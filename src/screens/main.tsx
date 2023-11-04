a3630989fde1189d58e4a410d0d863993747cae2
Gabriel Rodrigues <gabrielroodriz@gmail.com>
2023-11-03 21:09:04 -0300

    feat(app): Include Animated Checkbox

--- src/screens/main.tsx
+++ src/screens/main.tsx
@@ -1,9 +1,19 @@
 import { StatusBar } from 'expo-status-bar'
-import { Box, Center, Text, VStack, useColorModeValue } from 'native-base'
+import {
+  Box,
+  Center,
+  Pressable,
+  Text,
+  VStack,
+  useColorModeValue
+} from 'native-base'
 import React from 'react'
+
+import AnimatedCheckbox from 'react-native-checkbox-reanimated'
 import ThemeToggle from '../components/theme-toggle'
 
 export default function Main() {
+  const [checked, setChecked] = React.useState(true)
   return (
     <Center
       flex={1}
@@ -16,6 +26,16 @@ export default function Main() {
         </Box>
       </VStack>
       <Text>Open up App.tsx to start working on your app!</Text>
+      <Box w={100} h={100}>
+        <Pressable onPress={() => setChecked(!checked)}>
+          <AnimatedCheckbox
+            checked={checked}
+            highlightColor="#4444ff"
+            checkmarkColor="#ffffff"
+            boxOutlineColor="#4444ff"
+          />
+        </Pressable>
+      </Box>
       <ThemeToggle />
       <StatusBar style="auto" />
     </Center>
